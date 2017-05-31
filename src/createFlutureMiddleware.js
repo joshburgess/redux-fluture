73bb7a0d48d3b6096b43a0cc17fe89d02cfbad6f
joshburgess <joshburgess.webdev@gmail.com>
2017-05-30 20:24:42 -0400

    Slight internal API change & extra comment

--- src/createFlutureMiddleware.js
+++ src/createFlutureMiddleware.js
@@ -1,12 +1,12 @@
 import { isFuture } from 'fluture/src/core'
 
-const getRejectHandler = dispatch => next => action =>
+const getRejectHandler = ({ dispatch, next, action }) =>
   error => error.type
-    ? dispatch(error)
+    ? dispatch(error)  // if "error" is an action, dispatch it
     : next(action)
 
 // dispatch is a property of the middlewareApi exposed by Redux's applyMiddleware
 export const createFlutureMiddleware = () => ({ dispatch }) => next => action =>
   isFuture(action)
-    ? action.fork(getRejectHandler(dispatch)(next)(action), dispatch)
+    ? action.fork(getRejectHandler({ dispatch, next, action }), dispatch)
     : next(action)
